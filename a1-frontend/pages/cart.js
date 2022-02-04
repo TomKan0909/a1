a2fbd6ea909e907b7d7336a765fe4017818549b8
Elson <elsonliang@gmail.com>
2022-02-03 20:55:41 -0500

    Implement header bar

---
+++ a1-frontend/pages/cart.js
@@ -0,0 +1,12 @@
+import HeaderBar from "../components/HeaderBar";
+import Head from 'next/head'
+
+function Cart() {
+    return (
+        <div>
+            <HeaderBar active={1}/>
+            Cart page
+        </div>
+    );
+}
+export default Cart;
