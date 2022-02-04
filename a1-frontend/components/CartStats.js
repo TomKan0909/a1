2187ba2bdd05b21f4ad955a1e7dc67e8db7e4d60
TomKan <tomkan0909@gmail.com>
2022-02-04 14:13:07 -0500

    Made CartStats prettier

--- a1-frontend/components/CartStats.js
+++ a1-frontend/components/CartStats.js
@@ -1,10 +1,10 @@
-import { Grid } from '@nextui-org/react';
+import { Grid, Tooltip } from '@nextui-org/react';
 import { Text } from '@nextui-org/react';
 import Image from 'next/image';
 import { Container, Row, Col } from '@nextui-org/react';
-import { Button } from '@nextui-org/react';
+import { Button, Card, Divider} from '@nextui-org/react';
 import { Input } from '@nextui-org/react';
-
+import {MoreCircle} from 'react-iconly'
 
 const css = {
     wrapper: {
@@ -31,35 +31,78 @@ function CartStats() {
 
             <Grid.Container justify="space-between">
                 <Grid>
-                    Subtotal
+                    <Text>
+                        Subtotal
+                    </Text>
                 </Grid>
                 <Grid>
-                    $100.00
-                </Grid>
-            </Grid.Container>
-            <Grid.Container justify="space-between">
-                <Grid>
-                    Discount
-                </Grid>
-                <Grid>
-                    -$20.00
+                    <Text>
+                        Ξ 4200   
+                    </Text>
                 </Grid>
             </Grid.Container>
             <Grid.Container justify="space-between">
                 <Grid>
-                    Tax
+                    <Grid.Container>
+                        <Grid>
+                            <Text>
+                                Discount
+                            </Text>
+                        </Grid>
+                        <Grid>
+                            <Tooltip placement="bottom" content={
+                                <Card css={{ w: "200px" }}>
+                                <Card.Header>
+                                <Text b>Available Discounts</Text>
+                                </Card.Header>
+                                <Divider/>
+                                <Card.Body css={{ py: '$10' }}>
+                                <Text h5>
+                                    0xl23k4jf34: - 20% 
+                                </Text>
+                                <Text h5>
+                                    0x23l4kjv96: - 30%  
+                                </Text>
+                                <Text h5>
+                                    0xdfglkj54v: - Ξ 50    
+                                </Text>
+                                </Card.Body>
+                            </Card>
+                            }>
+                                <MoreCircle fill="currentColor" filled/>
+                            </Tooltip>
+                        </Grid>
+                    </Grid.Container>
                 </Grid>
                 <Grid>
-                    $10.00
+                    <Text>
+                        - Ξ 200                       
+                    </Text> 
+                </Grid>
+            </Grid.Container>
+            <Grid.Container justify="space-between">
+                <Grid>
+                    <Text>
+                        Tax
+                    </Text>
+                </Grid>
+                <Grid>
+                    <Text>
+                        Ξ 20                       
+                    </Text> 
                 </Grid>
             </Grid.Container>
             <div style={css.seperator}></div>
             <Grid.Container justify="space-between">
                 <Grid>
-                    Total
+                    <Text>
+                        Total
+                    </Text>
                 </Grid>
                 <Grid>
-                    $90.00
+                    <Text>
+                        Ξ 4020                       
+                    </Text>
                 </Grid>
             </Grid.Container>
             <Grid.Container wrap="nowrap">
