1cdc74f5d908522eaedfa6d5a1498ff906149617
Elson <elsonliang@gmail.com>
2022-02-03 23:41:19 -0500

    Finish header and implement cart page skeleton

--- a1-frontend/pages/cart.js
+++ a1-frontend/pages/cart.js
@@ -1,11 +1,41 @@
 import HeaderBar from "../components/HeaderBar";
 import Head from 'next/head'
+import CartItem from "../components/CartItem";
+import CartStats from "../components/CartStats";
+import HeaderBarSpace from "../components/HeaderBarSpace";
+
+const css = {
+    wrapperLeft: {
+        backgroundColor: `pink`,
+        width: `70%`,
+        float: `left`,
+    },
+    wrapperRight: {
+        backgroundColor: `tan`,
+        position: `fixed`,
+        right: `0px`,
+        width: `30%`,
+        float: `left`,
+    },
+}
 
 function Cart() {
     return (
         <div>
+            <HeaderBarSpace/>
+            
+            <div style={css.wrapperLeft}>
+                <CartItem/>
+                <CartItem/>
+                <CartItem/>
+                <CartItem/>
+                <CartItem/>
+            </div>
+            <div style={css.wrapperRight}>
+                <CartStats/>
+            </div>
+
             <HeaderBar active={1}/>
-            Cart page
         </div>
     );
 }
