276be32d34458cfb4e2a5ef520c4d3a6959d61a1
Elson <elsonliang@gmail.com>
2022-02-04 19:44:28 -0500

    Improve header appearance

--- a1-frontend/components/HeaderBar.js
+++ a1-frontend/components/HeaderBar.js
@@ -11,8 +11,8 @@ const css = {
         height: '60px',
         display: 'flex',
         alignItems: 'center',
-        backgroundColor: '#444444',
-        borderBottom: '3px solid black'
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
     },
     text: {
         padding: `0px 40px`,
