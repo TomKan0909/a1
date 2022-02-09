2ce3668b57bffca58ed4d3902df4ac9d9e687f21
TomKan <tomkan0909@gmail.com>
2022-02-09 13:43:26 -0500

    modified componenets to not use hard coded

--- a1-server/src/app.js
+++ a1-server/src/app.js
@@ -5,22 +5,45 @@ import path from 'path';
 const app = express();
 const __dirname = path.resolve();
 app.use(express.static(__dirname + '/public'));
+const PORT = 5000;
 
 app.get('/', (req, res) => {
     res.send('Test');
 });
 
 app.get('/punks', (req, res) => {
+    const protocol = req.protocol;
+    const host = req.hostname;
+    const url = req.originalUrl;
+    const port = PORT;
+    DB.forEach((obj) =>{
+        if (!obj["img"].startsWith(`${protocol}://${host}:${port}`)){
+            obj["img"] = `${protocol}://${host}:${port}` + obj["img"]
+        }
+        console.log(obj["img"]);
+    })
+
+    // for (let obj in DB){
+    //     console.log(obj);
+    //     // obj["img"] = `${protocol}://${host}:${port}` + obj["img"]
+    //     console.log(obj["img"]);
+    // }    
+
+
+    const fullUrl = `${protocol}://${host}:${port}${url}`
+    // console.log(fullUrl);
+    // console.log(req.path);
     res.send(JSON.stringify(DB))
 })
 
 app.get('/punks/:id', (req, res) => {
-    const key = "punk" + req.params.id;
-    console.log(DB["punk2890"]);
-    if (key in DB) {
-        res.send(JSON.stringify(DB[key]));
+    const key = "CryptoPunk #" + req.params.id;
+    const punk = DB.find((item) => item.title == key);
+    if (punk){
+        res.send(JSON.stringify(punk));
     } else {
-        res.status(404).send('Unable to find requested resource');
+        res.status(404).send('Unable to find requested resource');       
     }
+
 })
-app.listen(5000, () => console.log('Yes'));
+app.listen(PORT, () => console.log('Yes'));
--- a1-server/src/db.js
+++ a1-server/src/db.js
@@ -1,94 +1,204 @@
-const DB = {
-    "punk2890": {
-        name: "CryptoPunk #2890",
+const DB = [
+    {
+        title: "CryptoPunk #2890",
         price: "Ξ 605", 
-        img: "/punk2890.png"   
+        img: "/punk2890.png",
+        metadata: {
+            id: "2890",
+            type: "Human",
+            count: "3",
+            accessories: "Frumpy Hair, Green Eye Shadow, Pipe" 
+        }   
     },
-    "punk3051": {
-        name: "CryptoPunk #3051",
+    {
+        title: "CryptoPunk #3051",
         price: "Ξ 84.95", 
-        img: "/punk3051.png"   
+        img: "/punk3051.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+        
     },
-    "punk3100": {
-        name: "CryptoPunk #3100",
+    {
+        title: "CryptoPunk #3100",
         price: "Ξ 4200", 
-        img: "/punk3100.png"   
+        img: "/punk3100.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
     },
-    "punk4085": {
-        name: "CryptoPunk #4085",
+    {
+        title: "CryptoPunk #4085",
         price: "Ξ 60.95", 
-        img: "/punk4085.png"   
+        img: "/punk4085.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        } 
     },
-    "punk5217": {
-        name: "CryptoPunk #5217",
+    {
+        title: "CryptoPunk #5217",
         price: "Ξ 2250", 
-        img: "/punk5217.png"   
+        img: "/punk5217.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk5790": {
-        name: "CryptoPunk #5790",
+    {
+        title: "CryptoPunk #5790",
         price: "Ξ 72.99", 
-        img: "/punk5790.png"   
+        img: "/punk5790.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk6487": {
-        name: "CryptoPunk #6487",
+    {
+        title: "CryptoPunk #6487",
         price: "Ξ 550", 
-        img: "/punk6487.png"   
+        img: "/punk6487.png", 
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk6965": {
-        name: "CryptoPunk #6965",
+    {
+        title: "CryptoPunk #6965",
         price: "Ξ 800", 
-        img: "/punk6965.png"   
+        img: "/punk6965.png", 
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk7252": {
-        name: "CryptoPunk #7252",
+    {
+        title: "CryptoPunk #7252",
         price: "Ξ 1600", 
-        img: "/punk7252.png"   
+        img: "/punk7252.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
     },
-    "punk7804": {
-        name: "CryptoPunk #7804",
+    {
+        title: "CryptoPunk #7804",
         price: "Ξ 4200", 
-        img: "/punk7804.png"   
+        img: "/punk7804.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
+
     },
-    "punk8082": {
-        name: "CryptoPunk #8082",
+    {
+        title: "CryptoPunk #8082",
         price: "Ξ 40", 
-        img: "/punk8082.png"   
+        img: "/punk8082.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk8348": {
-        name: "CryptoPunk #8348",
+    {
+        title: "CryptoPunk #8348",
         price: "Ξ 85", 
-        img: "/punk8348.png"   
+        img: "/punk8348.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk8357": {
-        name: "CryptoPunk #8357",
+    {
+        title: "CryptoPunk #8357",
         price: "Ξ 115", 
-        img: "/punk8357.png"   
+        img: "/punk8357.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk8620": {
-        name: "CryptoPunk #8620",
+    {
+        title: "CryptoPunk #8620",
         price: "Ξ 400", 
-        img: "/punk8620.png"   
+        img: "/punk8620.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
     },
-    "punk8805": {
-        name: "CryptoPunk #8805",
+    {
+        title: "CryptoPunk #8805",
         price: "Ξ 420", 
-        img: "/punk8805.png"   
+        img: "/punk8805.png",   
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk8857": {
-        name: "CryptoPunk #8857",
+    {
+        title: "CryptoPunk #8857",
         price: "Ξ 2000", 
-        img: "/punk8857.png"   
+        img: "/punk8857.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
     },
-    "punk9572": {
-        name: "CryptoPunk #9572",
+    {
+        title: "CryptoPunk #9572",
         price: "Ξ 99.95", 
-        img: "/punk9572.png"   
+        img: "/punk9572.png",   
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }
     },
-    "punk9998": {
-        name: "CryptoPunk #9998",
+    {
+        title: "CryptoPunk #9998",
         price: "Ξ 124457.0675", 
-        img: "/punk9998.png"   
+        img: "/punk9998.png",
+        metadata: {
+            id: "",
+            type: "",
+            count: "",
+            accessories: "" 
+        }   
     }
-}
+]
 
 export default DB;
